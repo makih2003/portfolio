cd618590e7e49b6166cc3e726929dab92ab905f4
Maki <makihosokawa@MacBook-Pro.local>
2023-08-30 18:39:26 -0700

    update my skills

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,14 +6,20 @@ const Home = () => {
   return (
     <div className="container home-page">
       <div className="text-zone">
-        <h1>Hi, I'm <span className="maki">Maki </span></h1>
+        <h1>
+          Hi, I'm <span className="maki">Maki </span>
+        </h1>
         <h2>A Computer Science Student at Simon Fraser University</h2>
-        <h3>Experienced in JavaScript / Java / C++ / ReactJS / HTML / CSS</h3>
-        <Link to="/portfolio" className="flat-button">See My Work</Link>
+        <h3>
+          Experienced in JavaScript / Java / C++ / ReactJS / HTML / CSS / Python
+        </h3>
+        <Link to="/portfolio" className="flat-button">
+          See My Work
+        </Link>
       </div>
       <Pic />
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
