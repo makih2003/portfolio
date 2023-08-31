df9d9b5bf4c5b04771678adba9b7edde7952c4a2
Maki <makihosokawa@MacBook-Pro.local>
2023-08-30 17:35:12 -0700

    update About page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,44 +1,51 @@
 import './index.scss'
 
 const About = () => {
-  return(
-    <div className='container about-page'>
-      <div className='text-zone'>
+  return (
+    <div className="container about-page">
+      <div className="text-zone">
         <h1>About Me</h1>
         <div className="work-experience">
           <h2>Work Experience</h2>
           <p>
             <span>Sales Representative | MC Net Works Japan Ltd.</span>
-            <br />2016 - 2018
-            <br />Hyogo, Japan
+            <br />
+            2016 - 2018
+            <br />
+            Hyogo, Japan
           </p>
         </div>
         <div className="education">
           <h2>Education</h2>
           <p>
-            <span className="uni-name">Computer Science at <a href="https://langara.ca/">Langara College</a>
-            </span>
-            <span className="detail"><br />GPA 4.00 / 4.33 & Dean's Honour Roll</span>
-            <br />January 2022 - April 2023
-            <br />Vancouver, Canada
-          </p>
-          <p>
-            <span>Computing Science at <a href="https://www.sfu.ca/">Simon Fraser University</a></span>
-            <br /> September 2023 - Present
-            <br />Burnaby, Canada
-          </p>
-          <p>
             <span>
-              Bachelor of Science in Computing Science
+              Bachelor of Science in Computing Science at{' '}
+              <a href="https://www.sfu.ca/">Simon Fraser University</a>
             </span>
-            <br /> Expected in 2026
-            <br />Burnaby, Canada
+            <br /> September 2023 - Summer 2026 (Expected)
+            <br />
+            Burnaby, Canada
+          </p>
+          <p>
+            <span className="uni-name">
+              Computer Science at{' '}
+              <a href="https://langara.ca/">Langara College</a>
+            </span>
+            <span className="detail">
+              <br />
+              Completed First Year, Earned 33 Credits
+              <br />
+              GPA 4.00 / 4.33 & Dean's Honour Roll
+            </span>
+            <br />
+            January 2022 - April 2023
+            <br />
+            Vancouver, Canada
           </p>
         </div>
       </div>
     </div>
   )
-  
 }
 
 export default About
