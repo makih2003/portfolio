46a4557867292190fcbb41e3400bae1ac8225489
Maki <makihosokawa@MacBook-Pro.local>
2023-09-05 17:06:09 -0700

    update poly description

--- src/components/Portfolio/PortfolioData/index.js
+++ src/components/Portfolio/PortfolioData/index.js
@@ -37,7 +37,8 @@ const portfolioData = [
   {
     image: polyCalculator,
     title: 'Polynomial Calculator',
-    description: 'coming soon',
+    description:
+      'Polynomial calculator users can interact on the console. Users can do multiple operations after entering two polynomials, such as addition, subtraction, and differentiation. A linked list is used as its data structure. Big five (copy constructor, copy assignment, destructor, move constructor, move assignment) is also implemented.',
     code: 'https://github.com/makih2003/polynomial-calculator.git',
     language: (
       <>
